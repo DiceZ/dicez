29ab88f22416189c461459f49f19dcf45584a7ef
TheHenker <henkergive@gmail.com>
2016-07-02 23:36:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
  recaptcha_sitekey: '6LexiiMTAAAAAPgk61SMBRTB27tlrzKl87arwrgN',  // <----- EDIT ME!
- redirect_uri: 'https://dicez.github.io/dicez/',
+ redirect_uri: 'https://dicez.manydice.ml',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://socket.moneypot.com',
